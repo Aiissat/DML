8ce5d666fe3fa202d78b0f0d2a77ac862119d0e0
Aiissat <m.aiissatoudiallo@gmail.com>
2024-09-17 22:49:39 +0000

    first commit

---
+++ table_orders.sql
@@ -0,0 +1,3 @@
+INSERT INTO Orders (Customer_id, Product_id, Order_date, Quantity, Total_amount)
+VALUES ('C01','P02',NULL, 2,'9198'),
+       ('C02','P01','2020-05-28' , 1,'3299');
---
+++ table_product.sql
@@ -0,0 +1,4 @@
+INSERT INTO Product (Product_id, Product_name, Category, Price) 
+VALUES ('P01','Samsung Galaxy S20', 'Smartphone', 3299),
+       ('P02','HP Notebook', 'PC', 4599);
+    
